202b1dcc8e51c8ee58b396eb8607d28b1c9d3a3a
asciisoup <nvladsii11@gmail.com>
2023-06-19 20:43:52 +0200

    fist pytest examples

---
+++ otus_qa_automation/pytest_fixtures_beginnig/0_pytest_intro/test_pytest_intro.py
@@ -0,0 +1,15 @@
+def test_one():
+    print(" >>> I'm test one!")
+
+
+def test_two():
+    pass
+
+
+class TestClass:
+
+    def test_one(self):
+        pass
+
+    def test_two(self):
+        pass
